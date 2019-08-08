19c18eaaf9ca24276a432322f8d8af4ba1e0dbda
Timothy Shores <timothymshores@gmail.com>
2019-08-08 17:34:54 -0500

    Create item.py

--- src/adv.py
+++ src/adv.py
@@ -5,27 +5,26 @@ from room import Room
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
-# Link rooms together
+def room_info(key):
+    room = {
+        'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+        'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+        'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+        'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+        'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
+    }
+    return room[key]
 
+
+# Link rooms together
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -41,8 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-player1 = Player("outside")
-player2 = Player(Room("outside"))
+player1 = Player('outside')
+print(room_info('outside'))
 
 # Write a loop that:
 #
@@ -55,26 +54,12 @@ player2 = Player(Room("outside"))
 #
 # If the user enters "q", quit the game.
 
-# print(chr(27) + "[2J")
-
 currently_playing = True
 
-room_name = room[repr(player1)].name
-room_description = room[repr(player1)].description
-
-print(room_name)
-print(room_description)
-
 while currently_playing:
-    current_room = f'{player1}'
-
-    print(f"\nCurrent room: {room[current_room].name}")
-    print(f"Description: {room[current_room].description}\n")
 
     user_action = input(
-        "Enter a direction \n - n for north, \n - e for east, \n - s for south and \n - w for west \n - q to quit: ").lower()
-
-    direction = ['']
+        "\nEnter a direction \n - n for north, \n - e for east, \n - s for south and \n - w for west \n - q to quit: \nYour input: ").lower()
 
     if user_action == 'q':
         print('Thanks for playing')
--- src/room.py
+++ src/room.py
@@ -3,18 +3,26 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f'Current Room: {self.name}, \nDescription: {self.description}'
 
     def __repr__(self):
-        return f'{repr(self.name)}, {repr(self.description)}'
+        return f'Room: {repr(self.name)}, \nDescription: {repr(self.description)}'
 
     def name(self):
         return self.name
 
     def description(self):
         return self.description
+
+    def north(self):
+        if (self.name == 'Outside Cave Entrance'):
+            self.name = "Foyer"
+            self.description = "Dim light filters in from the south. Dusty passages run north and east."
+        else:
+            print("Unable to move north")
